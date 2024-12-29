fb55fc4dd74c309af39189209a06f429ab95b39f
András Körmendi <psyplaysgames@gmail.com>
2024-12-29 20:43:13 +0100

    add invalid credentials

--- test/configs/environment.conf.js
+++ test/configs/environment.conf.js
@@ -1,6 +1,8 @@
-import "dotenv/config"
+import "dotenv/config";
 
 export const secrets = {
-  VALID_USERNAME: process.env.VALID_USERNAME || '<your_valid_username>',
-  VALID_PASSWORD: process.env.VALID_PASSWORD || '<your_valid_password>'
-}
+  VALID_USERNAME: process.env.VALID_USERNAME || "<your_valid_username>",
+  VALID_PASSWORD: process.env.VALID_PASSWORD || "<your_valid_password>",
+  INVALID_USERNAME: "invalid@username.xyz",
+  INVALID_PASSWORD: "invalid_password",
+};
